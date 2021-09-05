8164339f08d26c25a33bebeedb63c4f125a856eb
MalikaArora <61543416+MalikaArora@users.noreply.github.com>
2021-09-05 17:42:01 +0530

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,8 @@ import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home  from './HomeComponent';
+import {Switch, Route, Redirect, BrowserRouter} from 'react-router-dom';
 class Main extends Component{
 
   constructor(props){
@@ -11,7 +13,6 @@ class Main extends Component{
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null
     };
 
   } 
@@ -23,18 +24,24 @@ class Main extends Component{
     });
 
 }
-
+ 
   render(){
+
+    const HomePage = () => {
+      return <Home/>;
+    }
 
     return(
       <div>
+        <BrowserRouter>
         <Header />
-
-        <Menu dishes={ this.state.dishes} onClick={ (dishId) => this.onDishSelect( dishId ) } />
-        <div className="container">
-        <DishDetail dish={this.state.dishes.filter((dish)=> dish.id===this.state.selectedDish)[0]}/>
-        </div>
+        <Switch>
+          <Route path="/home" component={HomePage}/>
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
+        </BrowserRouter>
       </div>
     );
 
